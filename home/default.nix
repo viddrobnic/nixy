e19118f8fb2f00b6a5b63556841b9ecc57ec19ff
Vid Drobnič <me@viddrobnic.com>
2026-02-24 19:51:18 +0100

    add btop

--- home/default.nix
+++ home/default.nix
@@ -42,5 +42,14 @@
     silent = true;
   };
 
+  programs.btop = {
+    enable = true;
+    settings = {
+      color_theme = "gruvbox_dark_v2";
+      vim_keys = true;
+      update_ms = 1000;
+    };
+  };
+
   programs.home-manager.enable = true;
 }
