b4e4d2ba8e1a5c388c58bfed861dc6edfd018cef
Vid Drobnič <me@viddrobnic.com>
2026-04-25 20:17:13 +0200

    manage ssh config

--- home/default.nix
+++ home/default.nix
@@ -14,6 +14,7 @@
     ./neovim.nix
     ./opencode.nix
     ./devenv.nix
+    ./ssh.nix
   ];
 
   home.packages = [
---
+++ home/ssh.nix
@@ -0,0 +1,50 @@
+{ pkgs, lib, ... }:
+let
+  isDarwin = pkgs.stdenv.isDarwin;
+
+  # I haven't gotten macos to play nicely with 'confirm' option yet. Until then,
+  # just add keys to agent always.
+  addKeysToAgentConfirm = if isDarwin then "yes" else "confirm";
+
+  extraOptions = lib.mkIf isDarwin {
+    "UseKeychain" = "yes";
+  };
+in
+{
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
+  };
+
+  programs.ssh.matchBlocks = {
+    "github.com" = {
+      identityFile = "~/.ssh/id_ed25519_git";
+      identitiesOnly = true;
+      addKeysToAgent = "yes";
+
+      extraOptions = extraOptions;
+    };
+
+    personal = {
+      hostname = "49.13.94.210";
+      user = "root";
+      port = 2222;
+      identityFile = "~/.ssh/id_ed25519_server";
+      identitiesOnly = true;
+      addKeysToAgent = addKeysToAgentConfirm;
+
+      extraOptions = extraOptions;
+    };
+
+    nixer = {
+      hostname = "116.202.25.234";
+      user = "root";
+      port = 2222;
+      identityFile = "~/.ssh/id_ed25519_git";
+      identitiesOnly = true;
+      addKeysToAgent = addKeysToAgentConfirm;
+
+      extraOptions = extraOptions;
+    };
+  };
+}
